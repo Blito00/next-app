06103997688fcda1a4f34dfde06fe62b74b74f92
Blito00 <rodriguezpablo0904@gmail.com>
2024-04-09 12:39:49 -0300

    Next App added

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import {Montserrat, Roboto} from 'next/font/google'
+
+export const monserrat = Montserrat({subsets:['latin']})
+
+export const roboto = Roboto({
+    weight:['400', '700'],
+    subsets:['latin']
+})
